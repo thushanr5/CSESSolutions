cc22a73d6a94739c8909bd63cf3e0dd2e7724e84
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2024-02-14 12:56:42 -0500

    Create trailingzeros.cpp

---
+++ intro/trailingzeros.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int solve(){
+    int n;
+    cin >> n;
+    int count =0;
+
+    long long factorial = 1;
+
+    for(long long i=1; i <= n; i++){
+        factorial *= i;
+    }
+
+    printf("%lld\n", factorial);
+
+    while(factorial % 10 == 0){
+        factorial/=10;
+        count++;
+    }
+    
+    return count;
+}
+
+
+int main() {
+  solve();
+  return 0;
+}
